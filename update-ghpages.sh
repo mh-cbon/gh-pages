80686c50b4f225bac28c0208a3050661ff7488c4
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-20 17:39:17 +0200

    updates

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -72,7 +72,7 @@ git config user.email "${EMAIL}"
 
 (git branch -aav | grep gh-pages) && git checkout gh-pages | echo "not remote gh pages"
 (git branch -aav | grep gh-pages) || (git checkout -b gh-pages;\
- git rm --cached $(git ls-files); git commit -am clean) | echo "not remote gh pages"
+ git rm $(git ls-files); git commit -am clean) | echo "not remote gh pages"
 
 git branch -aav
 git status
