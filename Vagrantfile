b2668634f22a591eee29b184d94c27a017d9c325
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-20 15:56:47 +0000

    clean

--- Vagrantfile
+++
@@ -1,8 +0,0 @@
-
-Vagrant.configure("2") do |config|
-
-  config.vm.box = "debian/jessie64"
-  config.vm.synced_folder ".", "/vagrant", type: "rsync"
-  config.vm.network "forwarded_port", guest: 4000, host: 8080
-
-end
--- all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget -O - https://raw.githubusercontent.com/mh-cbon/gh-pages/master/setup.sh | sh -x
-source ~/.rvm/scripts/rvm
-wget -O - https://raw.githubusercontent.com/mh-cbon/gh-pages/master/update-ghpages.sh | sh -x
